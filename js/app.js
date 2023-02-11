025c3c44652d43f1fca34a0fc0b038c5c5288bc8
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-02-11 21:53:11 +0600

    added js and css

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// add click handler with the submit button
+document.getElementById("submit-btn").addEventListener("click", function () {
+  // email field
+  const emailField = document.getElementById("email");
+  const email = emailField.value;
+  //   password field
+  const passwordField = document.getElementById("password");
+  const password = passwordField.value;
+  console.log(email, password);
+
+  //   validation check
+  if (email === "sumo@gmail.com" && password === "525252") {
+    console.log("valid user");
+  } else {
+    console.log("invalid user");
+  }
+});
